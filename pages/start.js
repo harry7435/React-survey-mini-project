50b231432d2bfe0f0ae34eb231faa92294aec43a
ikjun <harry960629@naver.com>
2023-04-27 17:13:05 +0900

    initital commit

---
+++ pages/start.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function start() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h3 className="title">
+          제로베이스님, 입사를 축하드립니다! 원하시는 개발 장비를 선택해주세요.
+        </h3>
+      </div>
+    </div>
+  );
+}
